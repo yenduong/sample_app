58e6fd9aafa90e1148248e84961f2cff0f76af71
Yen Duong <vicduong33@gmail.com>
2014-09-30 22:55:59 +1000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_secure_password
+	#has_secure_password
     before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
